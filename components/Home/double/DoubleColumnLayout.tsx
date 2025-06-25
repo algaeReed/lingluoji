98c85af354b59cb6f250502a3d055491aa09057f
张哲 <jefferylong@163.com>
2025-06-26 00:20:55 +0800

    feat：瀑布流布局

--- components/Home/double/DoubleColumnLayout.tsx
+++ components/Home/double/DoubleColumnLayout.tsx
@@ -1,27 +1,45 @@
 // components/Home/layouts/DoubleColumnLayout.tsx
+import { Item } from "@/store/itemStore";
+import MasonryList from "@react-native-seoul/masonry-list";
 import React from "react";
-import { FlatList, RefreshControl, View } from "react-native";
+import { View } from "react-native";
 import ItemCardForDouble from "./ItemForDouble";
 
-// TOODO改为瀑布流
-/**
- * 双列布局
- * @param param0
- * @returns
- */
-export default function DoubleColumnLayout({ items, refreshing, onRefresh, onEdit, onDelete }: any) {
+interface Props {
+  items: Item[];
+  refreshing: boolean;
+  onRefresh: () => void;
+  onEdit: (id: string) => void;
+  onDelete: (id: string) => void;
+}
+
+export default function DoubleColumnLayout({ items, refreshing, onRefresh, onEdit, onDelete }: Props) {
   return (
-    <FlatList
+    <MasonryList
       data={items}
-      numColumns={2}
-      columnWrapperStyle={{ justifyContent: "space-between", paddingHorizontal: 12 }}
       keyExtractor={(item) => item.id}
-      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-      renderItem={({ item }) => (
-        <View style={{ flex: 1, margin: 6 }}>
-          <ItemCardForDouble item={item} onEdit={onEdit} onDelete={onDelete} />
-        </View>
-      )}
+      numColumns={2}
+      showsVerticalScrollIndicator={false}
+      refreshing={refreshing}
+      onRefresh={onRefresh}
+      contentContainerStyle={{ paddingHorizontal: 12, paddingTop: 12 }}
+      renderItem={({ item }) => {
+        const typedItem = item as Item;
+        return (
+          // <View style={{ margin: 6 }}>
+          <View
+            style={{
+              margin: 6,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 1 },
+              shadowOpacity: 0.1,
+              shadowRadius: 2,
+            }}
+          >
+            <ItemCardForDouble item={typedItem} onEdit={onEdit} onDelete={onDelete} />
+          </View>
+        );
+      }}
     />
   );
 }
